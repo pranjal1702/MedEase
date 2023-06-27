31602c4a132351493fe30f60f42413a1b8d37c97
pranjal1702 <101694156+pranjal1702@users.noreply.github.com>
2023-06-27 14:28:56 +0530

    Update db.js

--- MedEase_website/config/db.js
+++ MedEase_website/config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("mongodb+srv://pranjalsingh1702:558HIkzBRwRIU3QY@cluster0.gy6owwt.mongodb.net/",{useUnifiedTopology: true,useNewUrlParser: true});
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
